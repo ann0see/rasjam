190ffdc98d29f010925ce10b136311a954efe694
ann0see <20726856+ann0see@users.noreply.github.com>
2020-09-10 16:51:28 +0200

    Bugfix cd

--- prepare_stick.sh
+++ prepare_stick.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 echo "This script compiles jamulus for the first time and downloads all dependencies,..."
 apt install git build-essential qtdeclarative5-dev qt5-default qttools5-dev-tools libjack-jackd2-dev
-startdir=$(pwd)
+# get script dir
+dir="$(dirname "$(readlink -f "$0")")"
 cd /tmp
 git clone https://github.com/corrados/jamulus.git
 cd jamulus
+# compile jamulus
 qmake Jamulus.pro
 make clean
 make
-cd $startdir
-mkdir copyToStick/files/Jamulus/
-cp Jamulus copyToStick/files/Jamulus/
+
+mkdir "${dir}/copyToStick/files/Jamulus/"
+cp Jamulus "${dir}/copyToStick/files/Jamulus/"
